97bb4655828476bfe133acf750b3de1be67b8458
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-09-10 17:29:03 -0300

    add controller with string and object responses

--- pet_api/src/main/java/com/daniel/pets/controller/PetController.java
+++ pet_api/src/main/java/com/daniel/pets/controller/PetController.java
@@ -17,38 +17,44 @@ public class PetController {
     private final PetService petService;
 
     @PostMapping
-    public ResponseEntity<Void> save(@RequestBody Pet pet){
-        petService.salvarPet(pet);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<?> save(@RequestBody Pet pet){
+        Pet savedPet = petService.salvarPet(pet);
+        return ResponseEntity.ok(new CustomResponse("Pet salvo com sucesso", savedPet));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Pet> findById(@PathVariable Integer id){
-        return ResponseEntity.ok(petService.buscarPetPorId(id));
+    public ResponseEntity<?> findById(@PathVariable Integer id){
+        Pet pet = petService.buscarPetPorId(id);
+        return ResponseEntity.ok(new CustomResponse("Pet encontrado", pet));
     }
 
     @GetMapping
-    public ResponseEntity<List<Pet>> findAll(){
-        return ResponseEntity.ok(petService.listarPets());
+    public ResponseEntity<?> findAll(){
+        List<Pet> pets = petService.listarPets();
+        return ResponseEntity.ok(new CustomResponse("Total de pets cadastrados: " + pets.size(), pets));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable Integer id){
+    public ResponseEntity<?> deleteById(@PathVariable Integer id){
         petService.deletarPetPorId(id);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(new CustomResponse("Pet deletado com sucesso", null));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Pet> replacePet (@PathVariable Integer id,
-                                             @RequestBody Pet novoPet){
-        petService.substituirPet(id, novoPet);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<?> replacePet (@PathVariable Integer id,
+                                         @RequestBody Pet novoPet){
+        Pet pet = petService.substituirPet(id, novoPet);
+        return ResponseEntity.ok(new CustomResponse("Pet substituído com sucesso", pet));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Pet> updatePet(@PathVariable Integer id,
-                                         @RequestBody Pet novoPet){
-        petService.atualizarPet(id, novoPet);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<?> updatePet(@PathVariable Integer id,
+                                       @RequestBody Pet novoPet){
+        Pet pet = petService.atualizarPet(id, novoPet);
+        return ResponseEntity.ok(new CustomResponse("Pet atualizado com sucesso", pet));
+    }
+
+    public record CustomResponse(String message, Object data) {
+
     }
 }
