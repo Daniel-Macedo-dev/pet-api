f08e88f112b00fe9ac7deee5f2bbd842c57369aa
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-09-16 00:28:55 -0300

    add Dono Service

---
+++ pet_api/src/main/java/com/daniel/pets/business/DonoService.java
@@ -0,0 +1,51 @@
+package com.daniel.pets.business;
+
+import com.daniel.pets.infrastructure.entities.Dono;
+import com.daniel.pets.infrastructure.repository.DonoReposository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DonoService {
+
+    private final DonoReposository donoReposository;
+
+    public DonoService(DonoReposository donoReposository) {this.donoReposository = donoReposository;}
+
+    public Dono salvarDono(Dono dono){return donoReposository.saveAndFlush(dono);}
+
+    public List<Dono> listarDonos(){return donoReposository.findAll();}
+
+    public Dono buscarDonoPorId(Integer id){
+        return donoReposository.findById(id).orElseThrow(
+                () -> new RuntimeException("id não encontrado")
+        );
+    }
+
+    public Dono substituirDono(Integer id, Dono novoDono){
+        Dono donoExistente = donoReposository.findById(id).orElseThrow(
+                () -> new RuntimeException("id não encontrado")
+        );
+        donoExistente.setNome(novoDono.getNome());
+        donoExistente.setEmail(novoDono.getEmail());
+        donoExistente.setTelefone(novoDono.getTelefone());
+
+        return donoReposository.saveAndFlush(donoExistente);
+    }
+
+    public Dono atualizarDono(Integer id, Dono novoDono){
+        Dono donoExistente = donoReposository.findById(id).orElseThrow(
+                () -> new RuntimeException("id não encontrado")
+        );
+        donoExistente.setEmail(novoDono.getEmail());
+        donoExistente.setTelefone(novoDono.getTelefone());
+
+        return donoReposository.saveAndFlush(donoExistente);
+    }
+
+    public void deletarDonoPorId(Integer id){donoReposository.deleteById(id);}
+
+
+
+}
