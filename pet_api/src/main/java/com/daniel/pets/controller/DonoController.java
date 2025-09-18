f39cc92b31f7e7932e58cef41d4589c5825958ff
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-09-18 06:28:53 -0300

    Adjustments

--- pet_api/src/main/java/com/daniel/pets/controller/DonoController.java
+++ pet_api/src/main/java/com/daniel/pets/controller/DonoController.java
@@ -34,6 +34,7 @@ public class DonoController {
         return ResponseEntity.ok(donoService.listarDonos());
     }
 
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(Integer id){
         return ResponseEntity.noContent().build();
